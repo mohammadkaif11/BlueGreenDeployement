3752bb1759fe5eb1bf74245849888f8aee484ab3
mohammadkaif <mohammadkaif0211@gmail.com>
2022-10-06 13:32:51 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+const port=process.env.port|| 8080;
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.get('/kaif', function(req, res){
+    res.send("hii sharding developer");
+ });
+
+ app.get('/sohail', function(req, res){
+    res.send("hii Post");
+ });
+
+app.listen(port,()=>{
+    console.log(`server is listen at ${port}`)
+});
